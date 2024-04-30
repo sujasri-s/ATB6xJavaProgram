a07959209b6ff4ececb79c409a578846318e6990
Suja Sriman <sujamaan98@gmail.com>
2024-04-30 16:13:43 +0530

    first commit

---
+++ src/ex_09042024/Lab001.java
@@ -0,0 +1,7 @@
+package ex_09042024;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println("Hello World !!");
+    }
+}
---
+++ src/ex_11042024/Lab002.java
@@ -0,0 +1,16 @@
+package ex_11042024;
+
+// This is single line comment
+/*
+* This is multi line comment
+* This is a sample program
+* */
+
+public class Lab002 {
+    public static void main(String[] args) {
+        System.out.println("Welcome");
+        System.out.print("ToJava");
+        System.out.print("Tutorial");
+        System.out.print("\nThis is the first class");
+    }
+}
