a1feb2bdf3b7a32c72eb95a6df11735d00a4c730
sujasri-s <sujamaan98@gmail.com>
2024-04-30 21:35:15 +0530

    commit 1

---
+++ src/ex_13042024/Lab008.java
@@ -0,0 +1,19 @@
+package ex_13042024;
+
+public class Lab008 {
+    public static void main(String[] args) {
+        int a = 10, b= 24;
+        String name = "Suja";
+        // Concatenation
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(name);
+        System.out.println(a+b);
+        System.out.println(a+name);
+        System.out.println(b+name);
+        System.out.println(name+a);
+        System.out.println(name+b);
+        System.out.println(name+a+b);
+        System.out.println(a+b+name);
+    }
+}
