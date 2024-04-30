c68fcfd0213505dbe0f15d11e61220e06eb50237
sujasri-s <sujamaan98@gmail.com>
2024-04-30 20:25:58 +0530

    commit 1

---
+++ src/ex_13042024/Lab004.java
@@ -0,0 +1,20 @@
+package ex_13042024;
+
+public class Lab004 {
+
+    //Primitive datatype
+    /*
+     * byte, short, int, long, char, float, double, boolean(true/false)
+     */
+
+    //Non-primitive datatype
+    /*
+     * String, Array, Class
+     */
+    public static void main(String[] args) {
+        // datatype variable_name/identifier = value;
+        int age = 56;
+        float quan = 3.14f;
+        System.out.println(age + quan);
+    }
+}
